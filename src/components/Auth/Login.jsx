2efe6f4dd88083e72e543857791419c821a04a4f
Hardik5862 <67375963+Hardik5862@users.noreply.github.com>
2021-02-18 14:51:00 +0530

    adding login component

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,9 +1,90 @@
-import { Component } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import firebase from "../../firebase";
+import {
+  Grid,
+  Form,
+  Segment,
+  Button,
+  Header,
+  Message,
+  Icon,
+} from "semantic-ui-react";
 
-class Login extends Component {
-  render() {
-    return <div>Login</div>;
-  }
-}
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setErrors([]);
+    setLoading(true);
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((signedInUser) => {
+        setLoading(false);
+        console.log(signedInUser);
+      })
+      .catch((err) => {
+        setLoading(false);
+        setErrors(errors.concat(err));
+      });
+  };
+
+  const displayErrors = (errors) =>
+    errors.map((error, i) => <p key={i}>{error.message}</p>);
+
+  return (
+    <Grid textAlign="center" verticalAlign="middle" className="app">
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" icon color="orange" textAlign="center">
+          <Icon name="rocketchat" color="orange" />
+          Login to Super Chat
+        </Header>
+        <Form onSubmit={handleSubmit} size="large">
+          <Segment stacked>
+            <Form.Input
+              fluid
+              name="email"
+              icon="mail"
+              iconPosition="left"
+              type="email"
+              placeholder="Email"
+              value={email}
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Form.Input
+              fluid
+              name="password"
+              icon="lock"
+              iconPosition="left"
+              type="password"
+              placeholder="Password"
+              value={password}
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button disabled={loading} color="orange" fluid size="large">
+              Submit
+            </Button>
+          </Segment>
+        </Form>
+        {errors.length ? (
+          <Message error>
+            <h3>Error</h3>
+            {displayErrors(errors)}
+          </Message>
+        ) : null}
+        <Message>
+          Dpn't have an account? <Link to="/register">Register</Link>
+        </Message>
+      </Grid.Column>
+    </Grid>
+  );
+};
 
 export default Login;
