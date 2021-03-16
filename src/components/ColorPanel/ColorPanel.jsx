f3e1df106e6830bd5f85c0fa6d76818969f1180e
Hardik5862 <67375963+Hardik5862@users.noreply.github.com>
2021-03-17 00:11:00 +0530

    fixing color panel error

--- src/components/ColorPanel/ColorPanel.jsx
+++ src/components/ColorPanel/ColorPanel.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React from "react";
+import firebase from "../../firebase";
 import { connect } from "react-redux";
 import { setColors } from "../../actions";
-import firebase from "../../firebase";
 import {
   Sidebar,
   Menu,
@@ -14,107 +14,126 @@ import {
 } from "semantic-ui-react";
 import { SliderPicker } from "react-color";
 
-const ColorPanel = ({ currentUser: user, setColors }) => {
-  const [modal, setModal] = useState(false);
-  const [primary, setPrimary] = useState("#40bf43");
-  const [secondary, setSecondary] = useState("#2d4d86");
-  const [userColors, setUserColors] = useState([]);
-  const usersRef = useMemo(() => {
-    return firebase.database().ref("users");
-  }, []);
+class ColorPanel extends React.Component {
+  state = {
+    modal: false,
+    primary: "",
+    secondary: "",
+    user: this.props.currentUser,
+    usersRef: firebase.database().ref("users"),
+    userColors: [],
+  };
 
-  const openModal = () => setModal(true);
-  const closeModal = () => setModal(false);
-
-  const handleChangePrimary = (color) => setPrimary(color.hex);
-  const handleChangeSecondary = (color) => setSecondary(color.hex);
-
-  useEffect(() => {
-    if (user) {
-      let userColors = [];
-      usersRef.child(`${user.uid}/colors`).on("child_added", (snap) => {
-        userColors.unshift(snap.val());
-        setUserColors(userColors);
-      });
+  componentDidMount() {
+    if (this.state.user) {
+      this.addListener(this.state.user.uid);
     }
-  }, [user, usersRef]);
+  }
 
-  const handleSaveColors = () => {
-    if (primary && secondary) {
-      saveColors(primary, secondary);
+  addListener = (userId) => {
+    let userColors = [];
+    this.state.usersRef.child(`${userId}/colors`).on("child_added", (snap) => {
+      userColors.unshift(snap.val());
+      this.setState({ userColors });
+    });
+  };
+
+  handleChangePrimary = (color) => this.setState({ primary: color.hex });
+
+  handleChangeSecondary = (color) => this.setState({ secondary: color.hex });
+
+  handleSaveColors = () => {
+    if (this.state.primary && this.state.secondary) {
+      this.saveColors(this.state.primary, this.state.secondary);
     }
   };
 
-  const saveColors = (primary, secondary) => {
-    usersRef
-      .child(`${user.uid}/colors`)
+  saveColors = (primary, secondary) => {
+    this.state.usersRef
+      .child(`${this.state.user.uid}/colors`)
       .push()
       .update({
         primary,
         secondary,
       })
       .then(() => {
-        console.log("colors added");
-        closeModal();
+        console.log("Colors added");
+        this.closeModal();
       })
-      .catch(console.log);
+      .catch((err) => console.error(err));
   };
 
-  const displayUserColors = (colors) =>
+  displayUserColors = (colors) =>
     colors.length > 0 &&
     colors.map((color, i) => (
       <React.Fragment key={i}>
         <Divider />
         <div
           className="color__container"
-          onClick={() => setColors(color.primary, color.secondary)}
+          onClick={() => this.props.setColors(color.primary, color.secondary)}
         >
           <div className="color__square" style={{ background: color.primary }}>
             <div
               className="color__overlay"
               style={{ background: color.secondary }}
-            ></div>
+            />
           </div>
         </div>
       </React.Fragment>
     ));
 
-  return (
-    <Sidebar
-      as={Menu}
-      icon="labeled"
-      inverted
-      vertical
-      visible
-      width="very thin"
-    >
-      <Divider />
-      <Button icon="add" size="small" color="blue" onClick={openModal} />
-      {displayUserColors(userColors)}
+  openModal = () => this.setState({ modal: true });
 
-      <Modal basic open={modal} onClose={closeModal}>
-        <Modal.Header>Choose App Colors</Modal.Header>
-        <Modal.Content>
-          <Segment inverted>
-            <Label content="Primary Color" />
-            <SliderPicker color={primary} onChange={handleChangePrimary} />
-          </Segment>
-          <Segment inverted>
-            <Label content="Secondary Color" />
-            <SliderPicker color={secondary} onChange={handleChangeSecondary} />
-          </Segment>
-        </Modal.Content>
-        <Modal.Actions>
-          <Button color="green" inverted onClick={handleSaveColors}>
-            <Icon name="checkmark" /> Save Colors
-          </Button>
-          <Button color="red" inverted onClick={closeModal}>
-            <Icon name="remove" /> Cancel
-          </Button>
-        </Modal.Actions>
-      </Modal>
-    </Sidebar>
-  );
-};
+  closeModal = () => this.setState({ modal: false });
+
+  render() {
+    const { modal, primary, secondary, userColors } = this.state;
+
+    return (
+      <Sidebar
+        as={Menu}
+        icon="labeled"
+        inverted
+        vertical
+        visible
+        width="very thin"
+      >
+        <Divider />
+        <Button icon="add" size="small" color="blue" onClick={this.openModal} />
+        {this.displayUserColors(userColors)}
+
+        {/* Color Picker Modal */}
+        <Modal basic open={modal} onClose={this.closeModal}>
+          <Modal.Header>Choose App Colors</Modal.Header>
+          <Modal.Content>
+            <Segment inverted>
+              <Label content="Primary Color" />
+              <SliderPicker
+                color={primary}
+                onChange={this.handleChangePrimary}
+              />
+            </Segment>
+
+            <Segment inverted>
+              <Label content="Secondary Color" />
+              <SliderPicker
+                color={secondary}
+                onChange={this.handleChangeSecondary}
+              />
+            </Segment>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button color="green" inverted onClick={this.handleSaveColors}>
+              <Icon name="checkmark" /> Save Colors
+            </Button>
+            <Button color="red" inverted onClick={this.closeModal}>
+              <Icon name="remove" /> Cancel
+            </Button>
+          </Modal.Actions>
+        </Modal>
+      </Sidebar>
+    );
+  }
+}
 
 export default connect(null, { setColors })(ColorPanel);
