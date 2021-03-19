16f431edf87ef54fe2913be2a009ac0a3356f872
Hardik5862 <67375963+Hardik5862@users.noreply.github.com>
2021-03-19 20:08:38 +0530

    setting up firebase for hosting and updating security rules

--- src/components/SidePanel/UserPanel.jsx
+++ src/components/SidePanel/UserPanel.jsx
@@ -130,7 +130,6 @@ class UserPanel extends React.Component {
       <Grid style={{ background: primaryColor }}>
         <Grid.Column>
           <Grid.Row style={{ padding: "1.2em", margin: 0 }}>
-            {/* App Header */}
             <Header inverted floated="left" as="h2">
               <Icon name="code" />
               <Header.Content>DevChat</Header.Content>
--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import "firebase/database";
 import "firebase/storage";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBNFyJpO-b1AjxENaiw_6EUg11Hw4_NlnI",
-  authDomain: "react-super-chat-460bb.firebaseapp.com",
-  projectId: "react-super-chat-460bb",
-  storageBucket: "react-super-chat-460bb.appspot.com",
-  messagingSenderId: "327979888496",
-  appId: "1:327979888496:web:e6ab1a6f4b2c688273a9b5",
-  measurementId: "G-0D23RD4W6N",
+  // firebse configs
 };
 
 firebase.initializeApp(firebaseConfig);
